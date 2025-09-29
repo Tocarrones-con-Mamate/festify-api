a3e626b92fdd1529680a346cdb67fd6a78b4033f
Raúl <raulgarcia6345@gmail.com>
2025-09-29 09:57:52 +0200

    Acto 3

--- src/main/java/web/controller/ArtistController.java
+++ src/main/java/web/controller/ArtistController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.TocarronesConMamate.festify_api.dto.response.ArtistResponse;
 import java.util.*;
@@ -7,8 +8,23 @@ import java.util.*;
 @RestController
 public class ArtistController {
 
+    private List<ArtistResponse> artists=List.of(
+        new ArtistResponse("1", "The Offspring", "Rock Punk", "Murica Land")
+    );   //Crea listas que no se pueden editar (o al menos no agregar cosas)
+
+    /*private List<ArtistResponse> artists;
+
+    public ArtistController() {
+        this.artists = new ArrayList<>();
+        this.artists.add(
+            new ArtistResponse("ART-001","Bad Bunny", "Pop","España")
+     );
+    }*/  //Crea listas que hay que editar con add
+
+    @GetMapping(path="/artists")
     public List<ArtistResponse> listArtist(){
-        return null;
+        return artists;
     }
+
 
 }
