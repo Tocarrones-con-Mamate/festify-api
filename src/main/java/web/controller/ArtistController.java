3e1d2e735ee351e6feae5632d6cca5e57ccec0d3
Raúl <raulgarcia6345@gmail.com>
2025-09-29 09:44:51 +0200

    Acto 2

--- src/main/java/web/controller/ArtistController.java
+++ src/main/java/web/controller/ArtistController.java
@@ -1,9 +1,14 @@
 package web.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import com.TocarronesConMamate.festify_api.dto.response.ArtistResponse;
+import java.util.*;
 
 @RestController
 public class ArtistController {
 
-    
+    public List<ArtistResponse> listArtist(){
+        return null;
+    }
+
 }
