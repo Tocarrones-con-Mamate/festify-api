e0d725e93304bf12dc48d87a13683cbe8bcb6a3d
Closarias <unax2015@gmail.com>
2025-09-29 09:44:45 +0200

    response

---
+++ src/main/java/com/TocarronesConMamate/festify_api/dto/response/ArtistResponse.java
@@ -0,0 +1,5 @@
+package com.TocarronesConMamate.festify_api.dto.response;
+
+public record ArtistResponse(String id, String name, String genre, String country) {
+    
+}
--- src/main/java/com/TocarronesConMamate/festify_api/web/controller/ArtistController.java
+++ src/main/java/com/TocarronesConMamate/festify_api/web/controller/ArtistController.java
@@ -1,13 +1,16 @@
 package com.TocarronesConMamate.festify_api.web.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
+import com.TocarronesConMamate.festify_api.dto.response.ArtistResponse;
 
 @RestController
 
 public class ArtistController {
     
-    public String getArtists() {
-        return "List of artists";
+    public List<ArtistResponse> listArtists() {
+        return null;
     }
 
 }
